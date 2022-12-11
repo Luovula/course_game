8919b84ef5809b37fb588b0448c898664e3ae639
Luna <luna.luovula@gmail.com>
2022-12-11 18:04:07 +0800

    interaction
    hp, attack, disappear, collision, damage

--- src/main.py
+++ src/main.py
@@ -11,8 +11,11 @@ class Luolataistelu:
 
         self.naytto = pygame.display.set_mode((640,480))
 
-        #robotin ja hirvion kohdat
+        #robotin ja hirvion kohdat ja hp
         self.kohdat = [[0,0 + self.seina.get_height()],[640-self.monsu.get_width(), 480-self.monsu.get_height()-self.seina.get_height()]]
+        self.hp = [20, 10]
+        
+        self.collide = False
 
         #pelissa kaytettavat napit
         self.napit = []
@@ -21,11 +24,13 @@ class Luolataistelu:
         self.napit.append((pygame.K_UP, 0, -3))
         self.napit.append((pygame.K_DOWN, 0, 3))
 
+        self.lyonti = [pygame.K_SPACE, 5]
+
         self.kello = pygame.time.Clock()
 
         self.painettu = {}
 
-        self.fontti = pygame.font.SysFont("Arial", 24)
+        self.fontti = pygame.font.SysFont("Arial", 13)
         pygame.display.set_caption("luolataistelu")
 
         self.silmukka()
@@ -33,9 +38,9 @@ class Luolataistelu:
 
 
     def lataa_kuvat(self):
-        self.robo = pygame.image.load("images/robo.png")
-        self.monsu = pygame.image.load("images/hirvio.png")
-        self.seina = pygame.image.load("seina/images.png")
+        self.robo = pygame.image.load("robo.png")
+        self.monsu = pygame.image.load("hirvio.png")
+        self.seina = pygame.image.load("seina.png")
 
     def silmukka(self):
         while True:
@@ -46,13 +51,28 @@ class Luolataistelu:
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.KEYDOWN:
                 self.painettu[tapahtuma.key] = True
- 
+                if tapahtuma.key == self.lyonti[0] and self.collide:
+                    self.hp[1] -= self.lyonti[1]
+
             if tapahtuma.type == pygame.KEYUP:
                 del self.painettu[tapahtuma.key]
 
+            if self.hp[1] <= 0:
+                self.kohdat[1][0] = -50
+                self.kohdat[1][1] = -50
+
             if tapahtuma.type == pygame.QUIT:
                 exit()
-            
+
+        if self.collide:
+                    self.hp[0] -= 3
+                    self.hp[1] += 3 
+
+        if (self.kohdat[0][0] >= self.kohdat[1][0] and self.kohdat[0][0] <= self.kohdat[1][0]+self.monsu.get_width()) and (self.kohdat[0][1] >= self.kohdat[1][1] and self.kohdat[0][1] <= self.kohdat[1][1] + self.monsu.get_height()):
+                self.collide = True
+        else:
+                self.collide = False   
+
         for nappi in self.napit:
             if nappi[0] in self.painettu:
                 self.kohdat[0][0] += nappi[1]
@@ -63,14 +83,15 @@ class Luolataistelu:
         self.kohdat[0][1] = max(self.kohdat[0][1] , 0 + self.seina.get_height())
         self.kohdat[0][1] = min(self.kohdat[0][1] , 480-self.robo.get_height()-self.seina.get_height())
 
-        if self.kohdat[1][0] > self.kohdat[0][0]:
-            self.kohdat[1][0] -= 1
-        if self.kohdat[1][0] < self.kohdat[0][0]:
-            self.kohdat[1][0] += 1
-        if self.kohdat[1][1] > self.kohdat[0][1]:
-            self.kohdat[1][1] -= 1
-        if self.kohdat[1][1] < self.kohdat[0][1]:
-            self.kohdat[1][1] += 1
+        if self.hp[1] > 0:
+            if self.kohdat[1][0] > self.kohdat[0][0]:
+                self.kohdat[1][0] -= 1
+            if self.kohdat[1][0] < self.kohdat[0][0]:
+                self.kohdat[1][0] += 1
+            if self.kohdat[1][1] > self.kohdat[0][1]:
+                self.kohdat[1][1] -= 1
+            if self.kohdat[1][1] < self.kohdat[0][1]:
+                self.kohdat[1][1] += 1
 
     def piirra_naytto(self):
         self.naytto.fill((150,75,0))
@@ -79,6 +100,10 @@ class Luolataistelu:
             self.naytto.blit(self.seina, (0 + i * self.seina.get_width(), 480-self.seina.get_height()))
         self.naytto.blit(self.robo,(self.kohdat[0][0], self.kohdat[0][1]))
         self.naytto.blit(self.monsu,(self.kohdat[1][0], self.kohdat[1][1]))
+        teksti = self.fontti.render("pelaaja hp: " + str(self.hp[0]), True, (255, 255, 255))
+        self.naytto.blit(teksti, (15, 15 + self.seina.get_height()))
+        teksti_2 = self.fontti.render("???: " + str(self.hp[1]), True, (0, 0, 0))
+        self.naytto.blit(teksti_2, (self.kohdat[1][0], self.kohdat[1][1] - 20))
         pygame.display.flip()
         self.kello.tick(60)
 if __name__ == "__main__":
